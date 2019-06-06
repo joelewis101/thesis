2c22de2ba546a9f403575133035a9099371003e3
joelewis101 <joelewis101@doctors.org.uk>
2019-06-06 14:51:03 +0100

    first edit without vpn

--- final_cleaning_scripts/load_and_clean_post72.R
+++ final_cleaning_scripts/load_and_clean_post72.R
@@ -3,30 +3,23 @@
 print("cleaning post72hr ......")
 
 library(RMySQL)
-library(plyr)
+#library(plyr)
 library(dplyr)
 library(reshape2)
-
-
-mydb = dbConnect(MySQL(), user='jlewis', password='jlewis@odk', dbname='odk_dassim', host='10.137.18.15')
-#dbListTables(mydb)
-rs = dbSendQuery(mydb, "select * from dassim_enrolment")
-enroll = fetch(rs, n=-1)
-dbClearResult(rs)
-dbDisconnect(mydb)
 
 
 post72 <- read.csv("/Users/joelewis/Documents/PhD/Data/Current/portal_downloads/assessment_post72hrs_raw.csv", stringsAsFactors = F)
 
 
 
+enroll.t <- enroll
+enroll.t$enroll_date <- enroll.t$data_date
 
-enroll$enroll_date <- enroll$data_date
+post72 <- merge(post72, select(enroll.t,pid, enroll_date), all.x = T)
+rm(enroll.t)
 
-post72 <- merge(post72, select(enroll,pid, enroll_date), all.x = T)
-
-post72$data_date <- as.Date(post72$data_date)
-post72$enroll_date <- as.Date(post72$enroll_date)
+post72$data_date <- as.Date(post72$data_date, "%d%b%Y")
+post72$enroll_date <- as.Date(post72$enroll_date, "%d%b%Y")
 
 # remove no PIDs
 
@@ -48,7 +41,10 @@ post72$data_date[post72$pid == "DAS1336U" & post72$data_date == "2017-10-13"] <-
 post72$data_date[post72$pid == "DAS1336U" & post72$data_date == "2017-10-10"] <- "2017-11-10"
 post72$data_date[post72$pid == "DAS1337S" & post72$data_date == "2017-10-14"] <- "2017-11-14"
 post72$data_date[post72$pid == "DAS1490A" & post72$data_date == "2010-06-19"] <- "2018-06-19"
-
+post72$data_date[post72$pid == "DAS1247W" & post72$data_date == "2018-01-01"] <- "2018-02-01"
+post72$data_date[post72$pid == "DAS15670" & post72$data_date == "2013-08-23"] <- "2018-08-23"
+post72$data_date[post72$pid == "DAS15670" & post72$data_date == "2013-08-24"] <- "2018-08-24"
+post72$data_date[post72$pid == "DAS15814" & post72$data_date == "2015-09-17"] <- "2018-09-17"
 post72$assess_type <- as.numeric(post72$data_date - post72$enroll_date)
 
 
@@ -60,7 +56,7 @@ post72$assess_type <- as.numeric(post72$data_date - post72$enroll_date)
 negative_assess_type <- post72$pid[post72$assess_type < 0]
 
 print("forms with date before enrollment follow")
-print(negative_assess_type)
+print(subset(post72, assess_type < 0))
 
 
 
@@ -71,11 +67,15 @@ print(negative_assess_type)
 
 removed_doubles <- c("297","296","318", "231","101","232","273","221","145","146",
                      "111","112","113","114","150","148","149","147","12","13","442","463","462", "589","527",
-                     "505","676", "552", "551", "549", "735")
+                     "505","676", "552", "551", "549", "735", "778", "153", "152", "832","830", "829",
+                     "831", "794", "795", "796", "797", "798", "799", "800", "543", "544", "545",
+                     "793", "804", "805", "806", "807", "808","809", "810", "812", "811", "813", "814",
+                     "815","816","817", "818", "819", "820", "821", "822", "823", "826", "827", "828", "825",
+                     "833", "834", "932", "934", "901", "902", "870", "879", "857")
 
 post72 <- subset(post72, !(row_id %in% removed_doubles))
 
-sm <- dcast(post72, pid ~ assess_type, fun.aggregate = length)
+sm <- dcast(post72, pid ~ assess_type, fun.aggregate = base::length)
 two.forms.one.day <- sm$pid[apply(sm[, -1], MARGIN = 1, function(x) any(x > 1))]
 
 tf <- subset(post72, pid %in% two.forms.one.day)
@@ -87,64 +87,4 @@ print(two.forms.one.day)
 cont <- readline("Continue?")
 if (cont == "N") {stop()}
 
-# how many are missing forms?
-# follow the pre 72 hr analysis
 
-
-mydb = dbConnect(MySQL(), user='jlewis', password='jlewis@odk', dbname='odk_dassim', host='10.137.18.15')
-dbListTables(mydb)
-rs = dbSendQuery(mydb, "select * from dassim_enrolment")
-enroll = fetch(rs, n=-1)
-dbClearResult(rs)
-rs = dbSendQuery(mydb, "select * from dassim_outcome")
-outcome = fetch(rs, n=-1)
-dbClearResult(rs)
-dbDisconnect(mydb)
-
-outcome$hospoutcomedate[is.na(outcome$hospoutcomedate)] <- outcome$data_date[is.na(outcome$hospoutcomedate)]
-
-missing <- select(enroll, pid, arm, data_date)
-missing <- subset(missing, arm == 1 | arm == 2)
-forms <- data.frame(table(post72$pid))
-names(forms) <- c("pid", "n_forms_in_db")
-
-missing <- merge(missing, forms, all.x = T)
-names(missing)[3] <- "enroll_date"
-missing$enroll_date <- as.Date(missing$enroll_date, "%Y-%m-%d")
-
-missing<- merge(missing, select(outcome,pid, hospoutcomedate), all.x = T)
-missing$hospoutcomedate[missing$pid == "DAS11625"] <- "2017-03-27"
-
-missing$hospoutcomedate <- as.Date(missing$hospoutcomedate, "%Y-%m-%d")
-
-missing$hospoutcomedate[is.na(missing$hospoutcomedate)] <- Sys.Date()
-
-missing$first_post72_form_date <- missing$enroll_date + 3
-
-missing$expected <- NA
-
-missing$first_post72_form_date[missing$first_post72_form_date > missing$hospoutcomedate] <-
-  missing$hospoutcomedate[missing$first_post72_form_date > missing$hospoutcomedate] # those with discharge before d3 have no forms
-  
-missing0 <- subset(missing, missing$first_post72_form_date <= missing$hospoutcomedate)
-
-Nweekdays <- Vectorize(function(a, b) 
-  sum(!weekdays(seq(a, b, "days")) %in% c("Saturday", "Sunday")))
-
-
-missing0$expected <- Nweekdays( missing0$first_post72_form_date, missing0$hospoutcomedate) 
-
-missing0$n_forms_in_db[is.na(missing0$n_forms_in_db)] <- 0
-missing0$missing <- missing0$expected - missing0$n_forms_in_db
-
-# generate a list of missing forms
-
-#names(missing0)[4] <- "n_forms"
-missing0 <- unique(missing0)
-missing <- (subset(missing0, missing > 0))
-missing <- subset(missing, arm == 1 | (arm ==2 & enroll_date >= "2017-09-12"))
-missing <- missing[order(missing$enroll_date),]
-
-write.csv(missing,"/Users/joelewis/Documents/PhD/datasets/post72/missing_post72.csv")
-
-write.csv(post72, "/Users/joelewis/Documents/PhD/datasets/post72/post72_clean.csv")
