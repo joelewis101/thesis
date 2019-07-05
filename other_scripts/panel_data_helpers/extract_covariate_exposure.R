573347a9a93f8573eb56963cd820cce3cd3329c9
joelewis101 <joelewis101@doctors.org.uk>
2019-07-05 14:51:31 +0100

    add starttime

--- other_scripts/panel_data_helpers/extract_covariate_exposure.R
+++ other_scripts/panel_data_helpers/extract_covariate_exposure.R
@@ -1,10 +1,13 @@
 ## extract covariate exposure
 
-extract_covariate_exposure <- function(dfin, a,b, endtime) {
+# expand df between covariate rows a and b (inclusive) and return a single row with
+# the total exposure between starttime and endtime
+
+extract_covariate_exposure <- function(dfin, a,b, starttime, endtime) {
   dfin <- as.data.frame(dfin)
   pid <- dfin$pid[1]
   dftemp <- uncompress_covariates(dfin)
-  dftemp <- subset(dftemp, assess_type <= endtime)
+  dftemp <- subset(dftemp, assess_type <= endtime & assess_type >= starttime)
   #apply(dftemp[,a:b],2, sum)
   cbind(pid,as.data.frame(t(apply(dftemp[,a:b],2, sum)))) -> dftemp
   dftemp$pid <- as.character(dftemp$pid)
